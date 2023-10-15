2566641365399fe32236fb57b9bb40ca2b00b720
Abdulrahman <test@test.com>
2023-10-15 13:12:38 +0300

    Putting the code for the project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Polygon {
+    int numofVertix;
+    int numofSides;
+    string name;
+};
+
+
+class Rectangle : public Polygon {
+
+};
+
+
+
+
+int main () {
+
+}
